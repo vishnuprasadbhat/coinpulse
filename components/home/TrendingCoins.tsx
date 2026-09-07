2baac7e59c9b7f819740677a83b1df26f476a415
Vishnuprasad Bhat <bhatvishnu.1@gmail.com>
2026-09-07 10:41:53 +0530

    implement coin overview and trending coins ui and data fetching logic

---
+++ components/home/TrendingCoins.tsx
@@ -0,0 +1,78 @@
+import { fetcher } from "@/lib/coingecko.actions";
+import DataTables from "../DataTables";
+import Link from "next/link";
+import Image from "next/image";
+import { TrendingDown, TrendingUp } from "lucide-react";
+import { cn, formatCurrency } from "@/lib/utils";
+
+const columns: DataTableColumn<TrendingCoin>[] = [
+  {
+    header: "Name",
+    cellClassName: "name-cell",
+    cell: (coin) => {
+      const item = coin.item;
+
+      return (
+        <Link href={`/coin/${item.id}`}>
+          <Image src={item.large} alt={item.name} width={36} height={36} />
+          <p>{item.name}</p>
+        </Link>
+      );
+    },
+  },
+  {
+    header: "24h Change",
+    cellClassName: "name-cell",
+    cell: (coin) => {
+      const item = coin.item;
+      const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
+
+      return (
+        <div
+          className={cn(
+            "price-change",
+            isTrendingUp ? "text-green-500" : "text-red-500",
+          )}
+        >
+          <p>
+            {isTrendingUp ? (
+              <TrendingUp width={16} height={16} />
+            ) : (
+              <TrendingDown width={16} height={16} />
+            )}
+          </p>
+          {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
+        </div>
+      );
+    },
+  },
+  {
+    header: "Price",
+    cellClassName: "price-cell",
+    cell: (coin) => formatCurrency(coin.item.data.price),
+  },
+];
+
+const TrendingCoins = async () => {
+  const trendingCoins = await fetcher<{ coins: TrendingCoin[] }>(
+    "/search/trending",
+    undefined,
+    300,
+  );
+
+  return (
+    <div id="trending-coins">
+      <h4>Trading Coins</h4>
+      <DataTables
+        data={trendingCoins.coins.slice(0, 6) || []}
+        columns={columns}
+        rowKey={(coin) => coin.item.id}
+        tableClassName="trending-coins-table"
+        headerCellClassName="py-3!"
+        bodyCellClassName="py-2!"
+      />
+    </div>
+  );
+};
+
+export default TrendingCoins;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(
+  value: number,
+  currency = "USD",
+  locale = "en-US",
+) {
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency,
+  }).format(value);
 }
