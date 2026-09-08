4f647d8abb333a747c0a74b0a9deb5c1de79ac5e
Vishnuprasad Bhat <bhatvishnu.1@gmail.com>
2026-09-08 15:33:17 +0530

    add candlestick chart component and integrate it into coin overview

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -2,13 +2,26 @@ import { fetcher } from "@/lib/coingecko.actions";
 import { formatCurrency } from "@/lib/utils";
 import Image from "next/image";
 import { CoinOverviewFallback } from "./fallback";
+import CandlestickCharts from "../CandlestickCharts";
 
 const CoinOverview = async () => {
-  let coin;
+  let coin: CoinDetailsData;
+  let coinOHLCData: OHLCData[];
+
   try {
-    coin = await fetcher<CoinDetailsData>("/coins/bitcoin", {
-      dex_pair_format: "symbol",
-    });
+    const [coinData, ohlcData] = await Promise.all([
+      fetcher<CoinDetailsData>("/coins/bitcoin", {
+        dex_pair_format: "symbol",
+      }),
+      fetcher<OHLCData[]>("/coins/bitcoin/ohlc", {
+        vs_currency: "usd",
+        days: 1,
+        precison: "full",
+        // interval: "daily",
+      }),
+    ]);
+    coin = coinData;
+    coinOHLCData = ohlcData;
   } catch (error) {
     console.error("Error fetching coin overview", error);
     return <CoinOverviewFallback />;
@@ -16,15 +29,22 @@ const CoinOverview = async () => {
 
   return (
     <div id="coin-overview">
-      <div className="header pt-2">
-        <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
-        <div className="info">
-          <p>
-            {coin.name} / {coin.symbol}
-          </p>
-          <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+      <CandlestickCharts data={coinOHLCData} coinId="bitcoin">
+        <div className="header pt-2">
+          <Image
+            src={coin.image.large}
+            alt={coin.name}
+            width={56}
+            height={56}
+          />
+          <div className="info">
+            <p>
+              {coin.name} / {coin.symbol}
+            </p>
+            <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+          </div>
         </div>
-      </div>
+      </CandlestickCharts>
     </div>
   );
 };
